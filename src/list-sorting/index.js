a33b2c46e2c0dd1b8f1a6d7dfe5c901669cc5ec5
seunodewale <seunodewale.zone5@gmail.com>
2020-05-07 22:06:39 +0100

    optimize solution

--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,21 +1,19 @@
 function listSorting(needle, haystack) {
   let output = -1;
+  let index = -1;
+  let arrayIndex;
 
   if(typeof haystack[0] === "number"){
 
     for(let i=0; i<haystack.length; i++){
-
       if( needle === haystack[i] ){
         output = i;
       }
-
     }
     return output;
   }
 
   output = [];
-  let index = -1;
-  let arrayIndex;
 
   for(let i=0; i<haystack.length; i++){
     for(let j=0; j<haystack[i].length; j++){
@@ -26,7 +24,7 @@ function listSorting(needle, haystack) {
     }
   }
   output.push(arrayIndex, index);
-  
+
   return index !== -1 ? output : index;
 
 }
